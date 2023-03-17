86b23691f193b51e8a193117173df20512029dfb
Maurox28363429 <maur28363429rd@gmail.com>
2023-03-17 12:04:23 -0400

    example

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,20 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn color="white" text-color="black" :label="maria" to="maria"/>
+    <q-btn color="primary" label="Primary" />
+    <q-btn color="secondary" label="Secondary" />
+    <q-btn color="amber" glossy label="Amber" />
+    <q-btn color="brown-5" label="Brown 5" />
+    <q-btn color="deep-orange" glossy label="Deep Orange" />
+    <q-btn color="purple" label="Purple" />
+    <q-btn color="black" label="Black" />
+  </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+<script setup>
+import { ref,onMounted } from 'vue'
+const maria=ref('maria');
+onMounted(()=>{
+  maria.value="bracho";
+});
 </script>
