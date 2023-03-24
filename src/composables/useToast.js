514a092fed1cfe45a643d7d0c28d5112a434e8df
maria Bracho <mariabrachovillanueva@gmail.com>
2023-03-24 12:36:59 -0400

    feat: :sparkles: Add userToast for handle Toast

---
+++ src/composables/useToast.js
@@ -0,0 +1,20 @@
+import { useQuasar } from 'quasar'
+
+export const useToast = () => {
+  const $q = useQuasar()
+  const triggerPositive = (message) => {
+    $q.notify({
+      type: 'positive',
+      message
+    })
+  }
+
+  const triggerWarning = (message) => {
+    $q.notify({
+      type: 'warning',
+      message
+    })
+  }
+
+  return { triggerPositive, triggerWarning }
+}
