ab45bad323d5c8da6ac6f94a0f66aee68d316f02
maria Bracho <mariabrachovillanueva@gmail.com>
2023-04-10 16:09:10 -0400

    hide in gitignore plugins and platforms

---
+++ src-cordova/platforms/android/app/src/main/assets/www/assets/use-quasar.7c5cf8d6.js
@@ -0,0 +1,1 @@
+import{ah as a,cc as r}from"./index.8e48f328.js";function u(){return a(r)}export{u};
--- src-cordova/www/assets/StorePage.31260766.js
+++
@@ -1,1 +0,0 @@
-import{cD as r}from"./index.159dbc85.js";const e={};function t(c,o){return"Store"}var a=r(e,[["render",t]]);export{a as default};
