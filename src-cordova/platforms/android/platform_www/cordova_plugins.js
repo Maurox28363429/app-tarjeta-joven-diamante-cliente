25c405245a19d383a79f61ef9dc5d4d4d91b860f
mauricio <maur28363429rd@gmail.com>
2023-04-28 15:15:48 -0400

    a

--- src-cordova/platforms/android/platform_www/cordova_plugins.js
+++ src-cordova/platforms/android/platform_www/cordova_plugins.js
@@ -1,59 +1,75 @@
-cordova.define("cordova/plugin_list", function (require, exports, module) {
+cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
-      id: "cordova-plugin-android-permissions.Permissions",
-      file: "plugins/cordova-plugin-android-permissions/www/permissions.js",
-      pluginId: "cordova-plugin-android-permissions",
-      clobbers: ["cordova.plugins.permissions"],
+      "id": "cordova-plugin-android-permissions.Permissions",
+      "file": "plugins/cordova-plugin-android-permissions/www/permissions.js",
+      "pluginId": "cordova-plugin-android-permissions",
+      "clobbers": [
+        "cordova.plugins.permissions"
+      ]
     },
     {
-      id: "cordova-plugin-camera.Camera",
-      file: "plugins/cordova-plugin-camera/www/CameraConstants.js",
-      pluginId: "cordova-plugin-camera",
-      clobbers: ["Camera"],
+      "id": "cordova-plugin-camera.Camera",
+      "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "Camera"
+      ]
     },
     {
-      id: "cordova-plugin-camera.CameraPopoverOptions",
-      file: "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
-      pluginId: "cordova-plugin-camera",
-      clobbers: ["CameraPopoverOptions"],
+      "id": "cordova-plugin-camera.CameraPopoverOptions",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverOptions"
+      ]
     },
     {
-      id: "cordova-plugin-camera.camera",
-      file: "plugins/cordova-plugin-camera/www/Camera.js",
-      pluginId: "cordova-plugin-camera",
-      clobbers: ["navigator.camera"],
+      "id": "cordova-plugin-camera.camera",
+      "file": "plugins/cordova-plugin-camera/www/Camera.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "navigator.camera"
+      ]
     },
     {
-      id: "cordova-plugin-camera.CameraPopoverHandle",
-      file: "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
-      pluginId: "cordova-plugin-camera",
-      clobbers: ["CameraPopoverHandle"],
+      "id": "cordova-plugin-camera.CameraPopoverHandle",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverHandle"
+      ]
     },
     {
-      id: "cordova-plugin-inappbrowser.inappbrowser",
-      file: "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-      pluginId: "cordova-plugin-inappbrowser",
-      clobbers: ["cordova.InAppBrowser.open"],
+      "id": "cordova-plugin-inappbrowser.inappbrowser",
+      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+      "pluginId": "cordova-plugin-inappbrowser",
+      "clobbers": [
+        "cordova.InAppBrowser.open"
+      ]
     },
     {
-      id: "cordova-plugin-splashscreen.SplashScreen",
-      file: "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-      pluginId: "cordova-plugin-splashscreen",
-      clobbers: ["navigator.splashscreen"],
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
     },
     {
-      id: "cordova-plugin-qrscanner.QRScanner",
-      file: "plugins/cordova-plugin-qrscanner/www/www.min.js",
-      pluginId: "cordova-plugin-qrscanner",
-      clobbers: ["QRScanner"],
-    },
+      "id": "cordova-plugin-qr-barcode-scanner.BarcodeScanner",
+      "file": "plugins/cordova-plugin-qr-barcode-scanner/www/barcodescanner.js",
+      "pluginId": "cordova-plugin-qr-barcode-scanner",
+      "clobbers": [
+        "cordova.plugins.barcodeScanner"
+      ]
+    }
   ];
   module.exports.metadata = {
     "cordova-plugin-android-permissions": "1.1.5",
     "cordova-plugin-camera": "6.0.1-dev",
     "cordova-plugin-inappbrowser": "5.0.0",
     "cordova-plugin-splashscreen": "6.0.2",
-    "cordova-plugin-qrscanner": "3.0.1",
+    "cordova-plugin-qr-barcode-scanner": "8.0.3"
   };
 });
