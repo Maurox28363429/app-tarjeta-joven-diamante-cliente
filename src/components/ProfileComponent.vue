9c08e397680c3a6d30b247b4ec0c5b03243cc7d2
mauricio <maur28363429rd@gmail.com>
2023-05-14 17:20:46 -0400

    a

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -300,12 +300,12 @@
 </template>
 
 <script setup>
-import { userAuth } from "src/composables/userAuth";
-import { ref, watch, defineProps, computed, watchEffect } from "vue";
-import { useValidateForm } from "src/composables/useValidateForm";
-import { updateProfileShema } from "src/schemas/updateProfileShema";
-import { useUpdateUserMutation } from "src/querys/userQuerys";
-import { useGetStates } from "src/querys/offersQuerys";
+import { userAuth } from 'src/composables/userAuth'
+import { ref, watch, defineProps, computed, watchEffect } from 'vue'
+import { useValidateForm } from 'src/composables/useValidateForm'
+import { updateProfileShema } from 'src/schemas/updateProfileShema'
+import { useUpdateUserMutation } from 'src/querys/userQuerys'
+import { useGetStates } from 'src/querys/offersQuerys'
 
 const {
   updatedUser,
@@ -313,54 +313,54 @@ const {
   isLoadingUser,
   isFetchingUser,
   isFetchedAfterMountUser,
-  isFetchedUser,
-} = userAuth();
+  isFetchedUser
+} = userAuth()
 
 const props = defineProps({
   user: {
     type: String,
-    required: true,
-  },
-});
+    required: true
+  }
+})
 
-const { data } = useGetStates();
+const { data } = useGetStates()
 
 const provinceOptions = computed(() =>
   data.value?.data.map((element) => {
-    return element.name;
+    return element.name
   })
-);
+)
 
-const isBusiness = props.user === "business";
+const isBusiness = props.user === 'business'
 
-let genderCurrent = { label: "", value: "" };
+let genderCurrent = { label: '', value: '' }
 
 const GENDER_OPTIONS = [
-  { label: "Mujer", value: 0 },
-  { label: "Hombre", value: 1 },
-];
+  { label: 'Mujer', value: 0 },
+  { label: 'Hombre', value: 1 }
+]
 
-const file = ref(userData.value?.img_url ?? "");
+const file = ref(userData.value?.img_url ?? '')
 
 const { useForm, validatInput, validateMessage, validateForm } =
-  useValidateForm({ initialValue: {}, schema: updateProfileShema });
+  useValidateForm({ initialValue: {}, schema: updateProfileShema })
 
-console.log(useForm.value, "value");
+console.log(useForm.value, 'value')
 
 watchEffect(() => {
   if (useForm.value?.provincia?.length > 2 && userData.value) {
-    useForm.value?.provincia.pop();
+    useForm.value?.provincia.pop()
   }
-});
+})
 
 watch([userData, isFetchedAfterMountUser, isFetchedUser], () => {
-  console.log(useForm.value, "value");
+  console.log(useForm.value, 'value')
   if (userData.value && !isFetchingUser.value) {
     genderCurrent = GENDER_OPTIONS.find((item) => {
-      return item.value === Number(userData.value?.sex);
-    });
-    file.value = userData.value?.img_url;
-    console.log(useForm.value, "value");
+      return item.value === Number(userData.value?.sex)
+    })
+    file.value = userData.value?.img_url
+    console.log(useForm.value, 'value')
     useForm.value = {
       provincia: Array.isArray(userData.value.provincia)
         ? userData.value.provincia
@@ -372,37 +372,37 @@ watch([userData, isFetchedAfterMountUser, isFetchedUser], () => {
       sex: genderCurrent,
       address: userData.value?.address,
       img: null,
-      dni: userData.value?.dni || "",
+      dni: userData.value?.dni || '',
       beneficiario_poliza_cedula:
-        userData.value?.beneficiario_poliza_cedula || "",
-      beneficiario_poliza_name: userData.value?.beneficiario_poliza_name || "",
-      fecha_nacimiento: userData.value?.fecha_nacimiento || "",
-    };
+        userData.value?.beneficiario_poliza_cedula || '',
+      beneficiario_poliza_name: userData.value?.beneficiario_poliza_name || '',
+      fecha_nacimiento: userData.value?.fecha_nacimiento || ''
+    }
   }
-});
+})
 
-const { isLoading, mutateAsync } = useUpdateUserMutation();
+const { isLoading, mutateAsync } = useUpdateUserMutation()
 
 const uploadImg = (event) => {
-  const image = event.target.files[0];
-  useForm.value.img = image;
-  file.value = URL.createObjectURL(image);
-};
+  const image = event.target.files[0]
+  useForm.value.img = image
+  file.value = URL.createObjectURL(image)
+}
 
 const handledUpdateUser = async () => {
-  validateForm();
+  validateForm()
 
   const values = {
     ...useForm.value,
     role_id: userData.value.role_id,
     active: userData.value.active,
     id: userData.value.id,
-    sex: useForm.value.sex?.value,
-  };
-  await mutateAsync({ data: values, id: userData.value.id });
+    sex: useForm.value.sex?.value
+  }
+  await mutateAsync({ data: values, id: userData.value.id })
 
-  updatedUser();
-};
+  updatedUser()
+}
 </script>
 
 <style scoped>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,179 +1,179 @@
-const clientAuthMeta = { requiresAuth: true, role: "cliente" };
-const bussinesAuthMeta = { requiresAuth: true, role: "empresa" };
+const clientAuthMeta = { requiresAuth: true, role: 'cliente' }
+const bussinesAuthMeta = { requiresAuth: true, role: 'empresa' }
 
 const routes = [
   {
-    path: "/",
+    path: '/',
     children: [
       {
-        name: "login",
-        path: "login",
-        component: () => import("pages/LoginPage.vue"),
+        name: 'login',
+        path: 'login',
+        component: () => import('pages/LoginPage.vue')
       },
       {
-        path: "",
-        component: () => import("pages/LoginPage.vue"),
+        path: '',
+        component: () => import('pages/LoginPage.vue')
       },
       {
-        name: "forgotpassword",
-        path: "forgotpassword",
-        component: () => import("pages/ForgotpasswordPage.vue"),
+        name: 'forgotpassword',
+        path: 'forgotpassword',
+        component: () => import('pages/ForgotpasswordPage.vue')
       },
       {
-        name: "register",
-        path: "register",
-        component: () => import("pages/RegisterPage.vue"),
+        name: 'register',
+        path: 'register',
+        component: () => import('pages/RegisterPage.vue')
       },
       {
-        name: "recoveryPassword",
-        path: "recoveryPassword",
-        component: () => import("pages/RecoverypasswordPage.vue"),
+        name: 'recoveryPassword',
+        path: 'recoveryPassword',
+        component: () => import('pages/RecoverypasswordPage.vue')
       },
       {
-        path: "cliente",
+        path: 'cliente',
         meta: clientAuthMeta,
-        component: () => import("layouts/MainClientLayout.vue"),
+        component: () => import('layouts/MainClientLayout.vue'),
         children: [
           {
-            name: "Offers",
-            path: "Offers/:countryName",
-            component: () => import("src/pages/client/OffersPage.vue"),
+            name: 'Offers',
+            path: 'Offers/:countryName',
+            component: () => import('src/pages/client/OffersPage.vue')
           },
           {
-            path: "OffersForUniversitys/:countryName",
-            component: () => import("src/pages/client/OffersForUniversity.vue"),
+            path: 'OffersForUniversitys/:countryName',
+            component: () => import('src/pages/client/OffersForUniversity.vue')
           },
           {
-            path: "Offers",
+            path: 'Offers',
             component: () =>
-              import("src/pages/client/SelectStateForffersPage.vue"),
-            props: { typeOffers: "Offers" },
+              import('src/pages/client/SelectStateForffersPage.vue'),
+            props: { typeOffers: 'Offers' }
           },
           {
-            path: "OffersForUniversitys",
+            path: 'OffersForUniversitys',
             component: () =>
-              import("src/pages/client/SelectStateForffersPage.vue"),
-            props: { typeOffers: "OffersForUniversitys" },
+              import('src/pages/client/SelectStateForffersPage.vue'),
+            props: { typeOffers: 'OffersForUniversitys' }
           },
           {
-            path: "pachama",
-            component: () => import("pages/client/PachamaPage.vue"),
+            path: 'pachama',
+            component: () => import('pages/client/PachamaPage.vue')
           },
           {
-            path: "contact",
-            component: () => import("pages/client/ContactPage.vue"),
+            path: 'contact',
+            component: () => import('pages/client/ContactPage.vue')
           },
           {
-            path: "memberships-type",
-            component: () => import("pages/client/MembershipsTypePage.vue"),
+            path: 'memberships-type',
+            component: () => import('pages/client/MembershipsTypePage.vue')
           },
           {
-            path: "",
-            component: () => import("pages/client/HomePage.vue"),
+            path: '',
+            component: () => import('pages/client/HomePage.vue')
           },
           {
-            path: "home",
-            component: () => import("pages/client/HomePage.vue"),
+            path: 'home',
+            component: () => import('pages/client/HomePage.vue')
           },
           {
-            path: "account",
+            path: 'account',
             children: [
               {
-                path: "",
-                component: () => import("components/UserAccount.vue"),
-                props: { user: "client" },
+                path: '',
+                component: () => import('components/UserAccount.vue'),
+                props: { user: 'client' }
               },
               {
-                path: "profile",
-                component: () => import("components/ProfileComponent.vue"),
-                props: { user: "client" },
-              },
-            ],
+                path: 'profile',
+                component: () => import('components/ProfileComponent.vue'),
+                props: { user: 'client' }
+              }
+            ]
           },
           {
-            name: "transactionsTable",
-            path: "transactionsTable",
-            component: () => import("components/TransactionsTable.vue"),
+            name: 'transactionsTable',
+            path: 'transactionsTable',
+            component: () => import('components/TransactionsTable.vue')
           },
           {
-            name: "promotions",
-            path: "promotions",
-            component: () => import("pages/client/PromotionsPage.vue"),
+            name: 'promotions',
+            path: 'promotions',
+            component: () => import('pages/client/PromotionsPage.vue')
           },
           {
-            name: "news",
-            path: "news",
-            component: () => import("pages/client/NewsPage.vue"),
-          },
-        ],
+            name: 'news',
+            path: 'news',
+            component: () => import('pages/client/NewsPage.vue')
+          }
+        ]
       },
       {
-        path: "empresa",
+        path: 'empresa',
         meta: bussinesAuthMeta,
-        component: () => import("layouts/MainBussinesLaout.vue"),
+        component: () => import('layouts/MainBussinesLaout.vue'),
         children: [
           {
-            path: "",
-            component: () => import("src/components/TransactionsTable.vue"),
+            path: '',
+            component: () => import('src/components/TransactionsTable.vue')
           },
           {
-            path: "account",
+            path: 'account',
             children: [
               {
-                path: "",
-                component: () => import("components/UserAccount.vue"),
-                props: { user: "business" },
+                path: '',
+                component: () => import('components/UserAccount.vue'),
+                props: { user: 'business' }
               },
               {
-                path: "profile",
-                component: () => import("components/ProfileComponent.vue"),
-                props: { user: "business" },
-              },
-            ],
+                path: 'profile',
+                component: () => import('components/ProfileComponent.vue'),
+                props: { user: 'business' }
+              }
+            ]
           },
           {
-            name: "orders",
-            path: "orders",
-            component: () => import("components/TransactionsTable.vue"),
+            name: 'orders',
+            path: 'orders',
+            component: () => import('components/TransactionsTable.vue')
           },
           {
-            name: "create-order",
-            path: "create-order",
-            component: () => import("pages/business/CreateOrderPage.vue"),
-          },
-        ],
+            name: 'create-order',
+            path: 'create-order',
+            component: () => import('pages/business/CreateOrderPage.vue')
+          }
+        ]
       },
       {
-        path: "memberships",
+        path: 'memberships',
         meta: clientAuthMeta,
         children: [
           {
-            path: "",
-            component: () => import("pages/client/MembershipsPage.vue"),
-            meta: clientAuthMeta,
-          },
-        ],
+            path: '',
+            component: () => import('pages/client/MembershipsPage.vue'),
+            meta: clientAuthMeta
+          }
+        ]
       },
       {
-        path: "memberships/:id",
-        component: () => import("pages/client/PaymentPage.vue"),
+        path: 'memberships/:id',
+        component: () => import('pages/client/PaymentPage.vue'),
         meta: clientAuthMeta,
         beforeEnter: (to, from, next) => {
           if (isNaN(parseInt(to.params.id))) {
-            next({ name: "error", params: { errorCode: 400 } });
+            next({ name: 'error', params: { errorCode: 400 } })
           } else {
-            next();
+            next()
           }
-        },
-      },
-    ],
+        }
+      }
+    ]
   },
   {
-    name: "error",
-    path: "/error/:errorCode",
-    component: () => import("pages/ErrorNotFound.vue"),
-    props: true,
-  },
-];
+    name: 'error',
+    path: '/error/:errorCode',
+    component: () => import('pages/ErrorNotFound.vue'),
+    props: true
+  }
+]
 
-export default routes;
+export default routes
