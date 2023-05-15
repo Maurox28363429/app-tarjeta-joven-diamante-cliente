3efb359b7542e72e0527e124eabb86cb356a4574
mauricio <maur28363429rd@gmail.com>
2023-05-15 17:55:42 -0400

    a

--- .quasar/client-entry.js
+++ .quasar/client-entry.js
@@ -11,55 +11,85 @@
  * Boot files are your "main.js"
  **/
 
-import { createApp } from "vue";
 
-import "@quasar/extras/line-awesome/line-awesome.css";
+import { createApp } from 'vue'
 
-import "@quasar/extras/roboto-font/roboto-font.css";
 
-import "@quasar/extras/material-icons/material-icons.css";
+
+
+
+
+
+import '@quasar/extras/line-awesome/line-awesome.css'
+
+import '@quasar/extras/roboto-font/roboto-font.css'
+
+import '@quasar/extras/material-icons/material-icons.css'
+
+
+
 
 // We load Quasar stylesheet file
-import "quasar/dist/quasar.sass";
+import 'quasar/dist/quasar.sass'
 
-import "src/css/app.scss";
 
-import "@quasar/quasar-ui-qiconpicker/src/index.sass";
 
-import createQuasarApp from "./app.js";
-import quasarUserOptions from "./quasar-user-options.js";
 
-console.info("[Quasar] Running SPA.");
+import 'src/css/app.scss'
 
-const publicPath = `/`;
+import '@quasar/quasar-ui-qiconpicker/src/index.sass'
 
-async function start({ app, router, store }, bootFiles) {
-  let hasRedirected = false;
-  const getRedirectUrl = (url) => {
-    try {
-      return router.resolve(url).href;
-    } catch (err) {}
 
-    return Object(url) === url ? null : url;
-  };
-  const redirect = (url) => {
-    hasRedirected = true;
+import createQuasarApp from './app.js'
+import quasarUserOptions from './quasar-user-options.js'
 
-    if (typeof url === "string" && /^https?:\/\//.test(url)) {
-      window.location.href = url;
-      return;
+
+
+
+
+
+console.info('[Quasar] Running SPA.')
+
+
+
+
+const publicPath = `/`
+
+async function start ({
+  app,
+  router
+  , store
+}, bootFiles) {
+  
+
+  
+  let hasRedirected = false
+  const getRedirectUrl = url => {
+    try { return router.resolve(url).href }
+    catch (err) {}
+
+    return Object(url) === url
+      ? null
+      : url
+  }
+  const redirect = url => {
+    hasRedirected = true
+
+    if (typeof url === 'string' && /^https?:\/\//.test(url)) {
+      window.location.href = url
+      return
     }
 
-    const href = getRedirectUrl(url);
+    const href = getRedirectUrl(url)
 
     // continue if we didn't fail to resolve the url
     if (href !== null) {
-      window.location.href = href;
-      window.location.reload();
+      window.location.href = href
+      window.location.reload()
     }
-  };
+  }
 
-  const urlPath = window.location.href.replace(window.location.origin, "");
+  const urlPath = window.location.href.replace(window.location.origin, '')
 
   for (let i = 0; hasRedirected === false && i < bootFiles.length; i++) {
     try {
@@ -70,55 +100,73 @@ async function start({ app, router, store }, bootFiles) {
         ssrContext: null,
         redirect,
         urlPath,
-        publicPath,
-      });
-    } catch (err) {
+        publicPath
+      })
+    }
+    catch (err) {
       if (err && err.url) {
-        redirect(err.url);
-        return;
+        redirect(err.url)
+        return
       }
 
-      console.error("[Quasar] boot error:", err);
-      return;
+      console.error('[Quasar] boot error:', err)
+      return
     }
   }
 
   if (hasRedirected === true) {
-    return;
+    return
   }
+  
 
-  app.use(router);
+  app.use(router)
+  
 
-  app.mount("#q-app");
+  
+
+    
+
+    
+      app.mount('#q-app')
+    
+
+    
+
+  
+
 }
 
-createQuasarApp(createApp, quasarUserOptions).then((app) => {
-  // eventually remove this when Cordova/Capacitor/Electron support becomes old
-  const [method, mapFn] =
-    Promise.allSettled !== void 0
+createQuasarApp(createApp, quasarUserOptions)
+
+  .then(app => {
+    // eventually remove this when Cordova/Capacitor/Electron support becomes old
+    const [ method, mapFn ] = Promise.allSettled !== void 0
       ? [
-          "allSettled",
-          (bootFiles) =>
-            bootFiles.map((result) => {
-              if (result.status === "rejected") {
-                console.error("[Quasar] boot error:", result.reason);
-                return;
-              }
-              return result.value.default;
-            }),
-        ]
-      : ["all", (bootFiles) => bootFiles.map((entry) => entry.default)];
+        'allSettled',
+        bootFiles => bootFiles.map(result => {
+          if (result.status === 'rejected') {
+            console.error('[Quasar] boot error:', result.reason)
+            return
+          }
+          return result.value.default
+        })
+      ]
+      : [
+        'all',
+        bootFiles => bootFiles.map(entry => entry.default)
+      ]
 
-  return Promise[method]([
-    import("boot/axios"),
+    return Promise[ method ]([
+      
+      import('boot/axios'),
+      
+      import('boot/vue-query'),
+      
+      import('@quasar/quasar-app-extension-qiconpicker/src/boot/register.js')
+      
+    ]).then(bootFiles => {
+      const boot = mapFn(bootFiles).filter(entry => typeof entry === 'function')
+      start(app, boot)
+    })
+  })
 
-    import("boot/vue-query"),
-
-    import("@quasar/quasar-app-extension-qiconpicker/src/boot/register.js"),
-  ]).then((bootFiles) => {
-    const boot = mapFn(bootFiles).filter(
-      (entry) => typeof entry === "function"
-    );
-    start(app, boot);
-  });
-});
