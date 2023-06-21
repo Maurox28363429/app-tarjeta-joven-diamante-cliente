909a8f056f23300be4b2f95bc0d0c7f9b02a7cdf
Maria Bracho <mariabrachovillanueva@gmail.com>
2023-06-21 13:27:08 -0400

    fix

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,6 @@ function alertMessage() {
     cancel: true,
   })
     .onOk(() => {
-      // console.log('OK')
       window.open(
         'https://play.google.com/store/apps/details?id=com.phoenixtechsa.app',
         '_system'
@@ -47,7 +46,6 @@ watchEffect(() => {
       if (
         Number(process.env.VUE_APP_VERSION) < Number(versionApp.value?.version)
       ) {
-        // console.log("hay una nueva version");
         alertMessage();
       } else {
         // console.log("no hay una nueva version");
