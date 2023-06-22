3cefbf3f707ae036cb4ef7a2397580103c3bc86c
mauricio <maur28363429rd@gmail.com>
2023-06-22 14:33:26 -0400

    as

--- src/pages/admin/MembresiasPage.vue
+++ src/pages/admin/MembresiasPage.vue
@@ -2,12 +2,14 @@
   <div class="text-center q-pa-md flex">
     <section style="width:100%">
       <q-card>
+        <h5 style="margin-left:1em;text-align:left;padding:1em">Membresias compradas</h5>
+        <hr>
         <table>
           <thead>
             <th>ID</th>
             <th>Nombre completo</th>
             <th>Membresia</th>
-            <th>Fecha de compra</th>
+            <th>Dias restantes</th>
             <th>Fecha de expiracion</th>
           </thead>
           <tbody>
@@ -15,19 +17,30 @@
               <td>{{d.id}}</td>
               <td>{{d.name}} {{d.last_name}}</td>
               <td>
+                <q-toggle
+                  v-model="d.membresia.status_num"
+                  @change="cambiar(d.membresia.id,d.membresia.status_num=!d.membresia.status_num)"
+                  :true-value="1"
+                  :false-value="0"
+                />
+              </td>
+              <td>
+                {{ d.membresia.days }}
+              </td>
+              <td>
+                {{ d.membresia.fecha_cobro }}
               </td>
             </tr>
           </tbody>
         </table>
+        <div class="flex flex-center" style="padding:1em">
+          <q-pagination
+            v-model="current"
+            :max="maxpage"
+            direction-links
+          />
+        </div>
       </q-card>
-      <div class="flex flex-center">
-        <q-pagination
-          v-model="current"
-          :max="maxpage"
-          direction-links
-          @input="cargar"
-        />
-      </div>
     </section>
   </div>
 </template>
@@ -40,16 +53,22 @@ td,th{
 }
 </style>
 <script setup>
-import { ref,onMounted } from 'vue';
+import { ref,onMounted, watch } from 'vue';
 import getUserMembresiaComprada from 'src/api/getUserMembresiaComprada.js'
 const current = ref(1);
 const maxpage = ref(1);
 const datos=ref([]);
+watch(current, async (val) => {
+  await cargar();
+});
 const cargar=async()=>{
   const {data,pagination}=await getUserMembresiaComprada(current.value)
   datos.value=data;
   current.value=pagination.currentPage;
   maxpage.value=pagination.lastPage;
+}
+const cambiar=async(id,status)=>{
+
 }
 onMounted(async () => {
   await cargar();
