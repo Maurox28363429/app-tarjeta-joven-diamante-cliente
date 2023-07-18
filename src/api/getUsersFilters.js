dd98d8cbe61f4062da46715dabab084e130708c5
mauricio <maur28363429rd@gmail.com>
2023-07-18 19:24:22 -0400

    as

---
+++ src/api/getUsersFilters.js
@@ -0,0 +1,6 @@
+import { instance } from '.';
+
+export default async function getUsersFilters(search) {
+  const { data } = await instance.get('/users?name=' + search+'&role_id=3&selected=1');
+  return data;
+}
